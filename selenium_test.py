51123887df68d3b4e5cf196b2e0cf4536e13f810
XS-Lin <34854151+XS-Lin@users.noreply.github.com>
2018-08-01 08:10:49 +0900

    Delete Local

--- selenium_test.py
+++ selenium_test.py
@@ -4,10 +4,7 @@ import xml.etree.ElementTree as et
 
 base_path = ""
 configFilePath = "config.xml"
-#base_path=r"C:\Users\linxu_000\Desktop\python script"
-#configFilePath=r"C:\Users\linxu_000\Desktop\python script\config_local.xml"
-#base_path=r"D:\Site\MyScript\python_test"
-#configFilePath=r"D:\Site\MyScript\python_test\config_local.xml"
+
 tree = et.ElementTree(file=configFilePath)
 root = tree.getroot()
 
