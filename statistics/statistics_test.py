467c21b86a867af6b75542b881f21a4fd66a7cd2
XS-Lin <34854151+XS-Lin@users.noreply.github.com>
2018-09-17 11:49:32 +0900

    jupyter notebook learning

--- statistics/statistics_test.py
+++ statistics/statistics_test.py
@@ -1,7 +1,8 @@
 # python 3.7
 import numpy
 import pandas
-sample_array = numpy.array([1,2,3,4,5])
-sample_array1 =sample_array + 2
-for x in sample_array1:
-    print(x)
+#sample_array = numpy.array([1,2,3,4,5])
+#sample_array1 =sample_array + 2
+#for x in sample_array1:
+#    print(x)
+sample_array = numpy.array([1,2,3,4,'A'])
